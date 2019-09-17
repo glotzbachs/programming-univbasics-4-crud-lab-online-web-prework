6eff3a58959ac427f42a2c6b09e5b158a4061216
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-09-17 16:20:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,15 +7,15 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  numbers.push(5)
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  numbers.unshift(0)
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  numbers.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
